27a4a66d54f7b814a494ec9e9178a383db05ba92
Jacek Kościesza <jacek.kosciesza@gmail.com>
2015-05-22 12:58:50 +0200

    Localization service test
    Using Xunit, but tests are not showing in 'Test runner' at the moment.

---
+++ Localization/Tests/WorkMarketingNet.Localization.Tests.Integration/LocalizationServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WorkMarketingNet.Localization.Tests.Integration
+{
+    public class LocalizationServiceTests
+    {
+		[Fact]
+		public void TrueIsTrue()
+		{
+			Assert.True(true);
+		}
+    }
+}
