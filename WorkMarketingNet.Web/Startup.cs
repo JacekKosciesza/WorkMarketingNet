3a07d36a85fa7df2bbf871135122c6eab7817b02
Jacek Kościesza <jacek.kosciesza@gmail.com>
2015-05-22 08:28:19 +0200

    MVC and static files added to request pipeline

--- WorkMarketingNet.Web/Startup.cs
+++ WorkMarketingNet.Web/Startup.cs
@@ -1,26 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
 
 namespace WorkMarketingNet.Web
 {
-    public class Startup
+	public class Startup
     {
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+			// Add MVC services to the services container.
+			services.AddMvc();
+		}
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerfactory)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+			// Configure the HTTP request pipeline.
+
+			// Add the console logger.
+			loggerfactory.AddConsole(minLevel: LogLevel.Warning);
+
+			// Add static files to the request pipeline.
+			app.UseStaticFiles();
+
+			// Add MVC to the request pipeline.
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action}/{id?}",
+					defaults: new { controller = "Home", action = "Index" });
+			});
+		}
     }
 }
