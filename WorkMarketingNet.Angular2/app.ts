3833275dfd1cf792a48b67c39f0458f7879a9fc9
Jacek Kościesza <jacek.kosciesza@gmail.com>
2015-07-18 12:59:23 +0200

    NgFor

--- WorkMarketingNet.Angular2/app.ts
+++ WorkMarketingNet.Angular2/app.ts
@@ -2,21 +2,32 @@
 
 // compile using: tsc -m commonjs -t es5 --emitDecoratorMetadata app.ts
 
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
 
 // Annotation section
 @Component({
   selector: 'my-app'
 })
 @View({
-  template: '<h1>Hello {{ name }}</h1>'
+  template: `
+  <h1>Hello {{ name }}</h1>
+  <p>Friends:</p>
+  <ul>
+     <li *ng-for="#name of names">
+        {{ name }}
+     </li>
+  </ul>
+  `,
+  directives: [NgFor]
 })
 // Component controller
 class MyAppComponent {
   name: string;
+  names: Array<string>;
   
   constructor() {
     this.name = 'Alice';
+    this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
   }
 }
 bootstrap(MyAppComponent);
